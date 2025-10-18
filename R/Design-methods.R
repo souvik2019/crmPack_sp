e8bc79871ebea8b2ba4b6bec9eae9c30b01fe918
Souvik Paul <souvikpaul1111@gmail.com>
2025-10-17 21:24:32 -0500

    Update Design-methods.R

--- R/Design-methods.R
+++ R/Design-methods.R
@@ -202,8 +202,8 @@ setMethod("simulate",
 
           stopit_results <- h_unpack_stopit(stopit)
           
-          cat("Simulation", iterSim, "stop_report:\n")
-          print(stopit_results)
+          # cat("Simulation", iterSim, "stop_report:\n")
+          # print(stopit_results)
         }
 
         ## get the fit
