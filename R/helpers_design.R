f3d59285261fea3e22a38e9dc9239e82adabb1a5
Souvik Paul <souvikpaul1111@gmail.com>
2025-11-12 20:43:35 -0600

    Update helpers_design.R

--- R/helpers_design.R
+++ R/helpers_design.R
@@ -270,6 +270,7 @@ h_determine_dlts <- function(data,
   } else {
     dltmat <- readRDS("dltmat_custom_code_scen1.rds")
     total_dlts = dltmat[iterSim,cohort_index]
+    cat('-sim-cohort-totaldlts-',iterSim,cohort_index,total_dlts)
     if(total_dlts == 0)
       dlts = c(0,0,0)
     if(total_dlts == 1)
