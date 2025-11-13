e9e61b6e9bcd01dba2fafe8e455d7f9b713cf4a1
Souvik Paul <souvikpaul1111@gmail.com>
2025-11-13 01:07:41 -0600

    Update Design-methods.R

--- R/Design-methods.R
+++ R/Design-methods.R
@@ -150,10 +150,7 @@ setMethod("simulate",
             cohort_size = thisSize,
             cohort_size_placebo = thisSize.PL,
             dose_grid = object@data@doseGrid[1],
-            first_separate = firstSeparate,
-            dltmat = dltmat,
-            iterSim = iterSim,
-            cohort_index = cohort_index
+            first_separate = firstSeparate
           )
 
           ## what is the dose limit?
@@ -201,12 +198,7 @@ setMethod("simulate",
           
           # Increment cohort index after processing all doses in this cohort
           cohort_index <- cohort_index + 1
-
-          #cat('Next recommended dose from crmpPack:', next_best_d$value,'\n')
-          #cat('Enter your next dose from custom code:','\n')
-          #dosemat <- readRDS("dosemat_custom_code_scen1.rds")
-          thisDose <- dosemat[iterSim, cohort_index] #next_best_d$value #scan(what = numeric(), quiet = TRUE)
-          #cat('-nextdose-',thisDose,'\n')
+          thisDose <- next_best_d$value 
           if(is.na(thisDose) & next_best_d$flag_non_na==1)
             cat('------- Warning: index mismatching, check Rules-Method.R line 226 --------','\n')
           
@@ -230,9 +222,6 @@ setMethod("simulate",
           )
 
           stopit_results <- h_unpack_stopit(stopit)
-          
-          # cat("Simulation", iterSim, "stop_report:\n")
-          # print(stopit_results)
         }
 
         ## get the fit
