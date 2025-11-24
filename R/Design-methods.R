8621df2fb1d5f84915463039e4dde0194b137384
Souvik Paul <souvikpaul1111@gmail.com>
2025-11-23 22:47:17 -0600

    Update Design-methods.R

--- R/Design-methods.R
+++ R/Design-methods.R
@@ -73,12 +73,12 @@ setMethod("simulate",
       nArgs <- max(nrow(args), 1L)
 
       ## seed handling
-      RNGstate <-  set_seed(seed) #as.integer(seed)  
+      RNGstate <-  as.integer(seed) #set_seed(seed)  
 
       ## from this,
       ## generate the individual seeds for the simulation runs
       set.seed(RNGstate)
-      simSeeds <- sample.int(n = 2147483647, size = as.integer(nsim)) #2147483647 987654321
+      #simSeeds <- sample.int(n = 2147483647, size = as.integer(nsim)) #2147483647 987654321
 
       ## the function to produce the run a single simulation
       ## with index "iterSim"
